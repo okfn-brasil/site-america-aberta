013bfb8d1057f17649526a5ade4d8423c4dc9526
Junior Rocha <jr@opara.me>
2024-08-28 21:55:12 -0300

    first commit and add css

---
+++ functions.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * America Aberta Theme functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package America Aberta
+ * @since 1.0.0
+ */
+
+/**
+ * Define Constants
+ */
+define( 'CHILD_THEME_AMERICA_ABERTA_VERSION', '1.0.0' );
+
+/**
+ * Enqueue styles
+ */
+function child_enqueue_styles() {
+
+	wp_enqueue_style( 'america-aberta-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_AMERICA_ABERTA_VERSION, 'all' );
+
+}
+
+add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
