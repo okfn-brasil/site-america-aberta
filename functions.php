d025cafc1dd13bacd63180c09c0e035a95a0cbae
Jr Rocha <jr@opara.me>
2024-09-11 17:36:22 -0300

    disable plugins and themes auto update

--- functions.php
+++ functions.php
@@ -23,3 +23,5 @@ function child_enqueue_styles() {
 }
 
 add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
+add_filter( 'auto_update_plugin', '__return_false' );
+add_filter( 'auto_update_theme', '__return_false' );
